ebdc0f1d870d6d861c6469b22c43a65bc0164052
antedesk <antedesk@gmail.com>
2016-07-07 12:37:26 +0200

    MVVM and data binding: example code

---
+++ HelloXamarin/HelloXamarin/FormViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloXamarin
+{
+    public class FormViewModel : INotifyPropertyChanged
+    {
+
+        string name, nickname, email;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public string Nickname
+        {
+            get { return nickname; }
+            set
+            {
+                if (nickname != value)
+                {
+                    nickname = value;
+                    OnPropertyChanged("Nickname");
+                }
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (email != value)
+                {
+                    email = value;
+                    OnPropertyChanged("Email");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var changed = PropertyChanged;
+            if (changed != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}

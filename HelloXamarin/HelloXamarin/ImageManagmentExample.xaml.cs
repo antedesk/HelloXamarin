261b8387b843482a8e6f174a6f796fc7fcf2c9ec
antedesk <antedesk@gmail.com>
2016-08-31 14:54:44 +0200

    Image managment with Xamarin.Froms

---
+++ HelloXamarin/HelloXamarin/ImageManagmentExample.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace HelloXamarin
+{
+    public partial class ImageManagmentExample : ContentPage
+    {
+
+        string logoUri = "http://www.html.it/wp-content/themes/www.html.it/images/logo.png";
+        public ImageManagmentExample()
+        {
+            InitializeComponent();
+
+            //impostazione dell'immagine lato code-behind per ogni piattaforma
+            img_codebehind.Source = Device.OnPlatform(ImageSource.FromFile("logo.png"),
+                                                 ImageSource.FromFile("logo.png"),
+                                                 ImageSource.FromFile("Images/logo.png"));
+
+            //caricamento di un immagine da URI
+            imageDownloaded.Source = ImageSource.FromUri(new Uri(logoUri));
+
+            imgCaching.Source = new UriImageSource
+            {
+                Uri = new Uri(logoUri),
+                CachingEnabled = true,
+                CacheValidity = new TimeSpan(5, 0, 0, 0)
+            };
+        }
+
+        void OnImageTapped(object sender, EventArgs args)
+        {
+            //cast dell'oggestto sender al tipo Image
+            Image image = (Image)sender;
+
+            //impostazione della opacità iniziale dell'immagine
+            image.Opacity = 0.75;
+
+            // rende l'immagine opaca per 100 ms
+            Device.StartTimer(TimeSpan.FromMilliseconds(2000), () =>
+            {
+                image.Opacity = 1;
+                return false;
+            });
+        }
+    }
+}
